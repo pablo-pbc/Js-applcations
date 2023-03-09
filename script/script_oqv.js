474f085ff6e8a6dc62b2ffd1691b48b9569fe264
pablo.carvalho <pablo.carvalho@icommgroup.com.br>
2023-03-09 15:06:07 -0300

    Tentativa de copiar o HTML do HomeSlide feminino

--- script/script_oqv.js
+++ script/script_oqv.js
@@ -1,39 +1,20 @@
-function getUserName() {
-    const firstPartImgOne = document.getElementById('firstPartImgOne').value
-    const secPartImgOne = document.getElementById('secPartImgOne').value
-    const thirdPartImgThree = document.getElementById('thirdPartImgThree').value
-    const altImgOne = document.getElementById('altImgOne').value
-    const linkImgOneDesk = document.getElementById('linkImgOneDesk').value
-    const linkImgOneMobile = document.getElementById('linkImgOneMobile').value
-    const linkImgOne = document.getElementById('linkImgOne').value
-
-    const resultFirstPartImgOne = document.querySelector('#mosaico-01-fem > a > div > div > p.pbc-hoover-title')
-    const resultSecPartImgOne = document.querySelector('#mosaico-01-fem > a > div > div > p.pbc-hoover-subtitle')
-    const resultThirdPartImgThree = document.querySelector('#mosaico-01-fem > a > div > div > p.pbc-hoover-text-bold')
-    const resultAltImgOne = document.querySelector('#mosaico-01-fem > a > picture > img')
-    const resultImgOneMobile = document.querySelector('#mosaico-01-fem > a > picture > source')
-    const resultLinkImgOne = document.querySelector('#mosaico-01-fem > a')
-
-
-    
-    if (firstPartImgOne.length < 3 && secPartImgOne < 3 && thirdPartImgThree < 3) {
-        alert('Favor preencher todos os campos corretamente');        
-    } else {
-        resultFirstPartImgOne.innerHTML = firstPartImgOne
-        resultSecPartImgOne.innerHTML = secPartImgOne
-        resultThirdPartImgThree.innerHTML = thirdPartImgThree
-        resultAltImgOne.alt = altImgOne
-        resultAltImgOne.src = linkImgOneDesk
-        resultImgOneMobile.srcset = linkImgOneMobile
-        resultLinkImgOne.href = linkImgOne
-    }    
-}
 
 function copy() {
-    var copyHtml = document.getElementById("mosaico-01-fem");
-    var z = copyHtml.outerHTML;
+    const nodeList = document.getElementsByClassName("box-banner");
 
-    document.getElementById("codigo").innerHTML = z;
+    console.log(nodeList)
+
+    let copyHtml = nodeList[0].outerHTML
+    document.getElementById("codigo").innerHTML = copyHtml
+
+    // for (let i = 0; i < nodeList.length; i++) {         
+        
+    //     let copyHtml = nodeList[i].outerHTML
+    //     document.getElementById("codigo").innerHTML = copyHtml
+    //     console.log(copyHtml)
+       
+    // }
+    // document.getElementById("codigo").innerHTML = pastHtml[0];
     document.getElementById("codigo").select();
     document.execCommand('copy');
 }
