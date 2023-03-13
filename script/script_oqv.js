80f89ebe431b7657dec6ea488c76ac5ca0b73023
pablo.carvalho <pablo.carvalho@icommgroup.com.br>
2023-03-13 12:35:56 -0300

    Adicionando função de atribuição de valores ao HTML

--- script/script_oqv.js
+++ script/script_oqv.js
@@ -18,3 +18,27 @@ function copy() {
 }
 
 //Função para preencher as informações do BANNER
+const bannerPosition = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
+const bannerPositonId = ['1', '2', '3', '4', '5', '6']
+
+function buildBannerFem(bannerPosition, bannerPositonId) {
+    //Importando o valor do input
+    let linkBannerFem = document.getElementById(`linkBanner${bannerPosition}Fem`).value
+    let bannerDeskFem = document.getElementById(`bannerDesk${bannerPosition}Fem`).value
+    let bannerMobileFem = document.getElementById(`bannerMobile${bannerPosition}Fem`).value
+    let altBannerFem = document.getElementById(`altBanner${bannerPosition}Fem`).value
+
+    //Selecionando o elemento HTML
+    let resultLinkBannerFem = document.querySelector(`#pbc-banner-fem-${bannerPositonId} > a`)
+    let resultBannerDeskFem = document.querySelector(`#pbc-banner-fem-${bannerPositonId} > a > picture > img`)
+    let resultBannerMobileFem = document.querySelector(`#pbc-banner-fem-${bannerPositonId} > a > picture > source`)
+
+    if (linkBannerFem.length < 3 && bannerDeskFem.length < 3 && bannerMobileFem.length < 3 && altBannerFem.length < 3) {
+        //alert('Favor preencher todos os campos corretamente');        
+    } else {
+        resultLinkBannerFem.href = linkBannerFem
+        resultBannerDeskFem.src = bannerDeskFem
+        resultBannerDeskFem.alt = altBannerFem
+        resultBannerMobileFem.srcset = bannerMobileFem
+    }  
+}
