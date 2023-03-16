d07fc81f87c4915d8bc99c811505045ff99c99d1
alexandre <alexandre.alves@mcx.com.br>
2023-03-16 15:45:30 -0300

    feature masc

--- OQV/script/homeSlideMasc.js
+++ OQV/script/homeSlideMasc.js
@@ -1,112 +1,208 @@
 //Function to feed the textarea element
 function loopTxtArea() {
-    const nodeList = document.getElementsByClassName("box-banner")
-    const txtArea = document.getElementById("codigo")
+  const nodeList = document.getElementsByClassName("box-banner");
+  const txtArea = document.getElementById("codigo");
 
-    txtArea.value = ''    
+  txtArea.value = "";
 
-    for (let i = 0; i < nodeList.length; i++) {         
-        let bannerNumComment = `<!-- 0${i+1} -->`
-        let bannerComment = `<!-- BANNER -->`
-        let copyHomeSlideHtml = nodeList[i].outerHTML
-        
-        document.getElementById("codigo").value += `${bannerNumComment}\n${bannerComment}\n${copyHomeSlideHtml}\n${bannerComment}\n\n`;
-    }
-    return txtArea
+  for (let i = 0; i < nodeList.length; i++) {
+    let bannerNumComment = `<!-- 0${i + 1} -->`;
+    let bannerComment = `<!-- BANNER -->`;
+    let copyHomeSlideHtml = nodeList[i].outerHTML;
+
+    document.getElementById(
+      "codigo"
+    ).value += `${bannerNumComment}\n${bannerComment}\n${copyHomeSlideHtml}\n${bannerComment}\n\n`;
+  }
+  return txtArea;
 }
 
 //Function to save the backup file
 function saveHomeSlideBackup() {
-    const date = new Date()    
-    const hours = date.getHours('pt-br')
-    const min = date.getMinutes('pt-br')
-    const today = date.toLocaleDateString('pt-br')
+  const date = new Date();
+  const hours = date.getHours("pt-br");
+  const min = date.getMinutes("pt-br");
+  const today = date.toLocaleDateString("pt-br");
 
-    const link = document.createElement("a")  
-    const file = new Blob([loopTxtArea().value], { type: 'text/plain' })
+  const link = document.createElement("a");
+  const file = new Blob([loopTxtArea().value], { type: "text/plain" });
 
-    link.href = URL.createObjectURL(file)
-    link.download = `backupBanner_${today}_${hours}h-${min}.txt`
-    link.click()
-    URL.revokeObjectURL(link.href)
+  link.href = URL.createObjectURL(file);
+  link.download = `backupBanner_${today}_${hours}h-${min}.txt`;
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
 
 //Function to copy the HTML after input's insertions
 function copyHomeSlideHtml() {
-    // Selecting the content inside the textarea
-    loopTxtArea().select()
-    // Selecting the content inside the textarea for mobile devices
-    loopTxtArea().setSelectionRange(0, 99999)
-    // Copy the text inside the textarea field
-    navigator.clipboard.writeText(loopTxtArea().value)     
+  // Selecting the content inside the textarea
+  loopTxtArea().select();
+  // Selecting the content inside the textarea for mobile devices
+  loopTxtArea().setSelectionRange(0, 99999);
+  // Copy the text inside the textarea field
+  navigator.clipboard.writeText(loopTxtArea().value);
 }
 
 //Function to add the Banner's informations
 function buildHomeSlide() {
-    const bannerPositonIdArray = ['1', '2', '3', '4', '5', '6']
-    let flag = 0
-    let i = 0
+  const bannerPositonIdArray = ["1", "2", "3", "4", "5", "6"];
+  let flag = 0;
+  let i = 0;
 
-    while (i < bannerPositonIdArray.length && flag < 1) {
-        let bannerPositonId = bannerPositonIdArray[i]
+  while (i < bannerPositonIdArray.length && flag < 1) {
+    let bannerPositonId = bannerPositonIdArray[i];
 
-        //Importing the input's values
-        let linkBannerMasc = document.getElementById(`linkBannerMasc${bannerPositonId}`).value
-        let bannerDeskMasc = document.getElementById(`bannerDeskMasc${bannerPositonId}`).value
-        let bannerMobileMasc = document.getElementById(`bannerMobileMasc${bannerPositonId}`).value
-        let altBannerMasc = document.getElementById(`altBannerMasc${bannerPositonId}`).value
+    //Importing the input's values
+    let linkBannerMasc = document.querySelector(
+      `#homeSlideForm > section:nth-child(${bannerPositonId}) > div:nth-child(2) > input[type=text]`
+    ).value;
+    let bannerDeskMasc = document.querySelector(
+      `#homeSlideForm > section:nth-child(${bannerPositonId}) > div:nth-child(3) > input[type=text]`
+    ).value;
+    let bannerMobileMasc = document.querySelector(
+      `#homeSlideForm > section:nth-child(${bannerPositonId}) > div:nth-child(4) > input[type=text]`
+    ).value;
+    let altBannerMasc = document.querySelector(
+      `#homeSlideForm > section:nth-child(${bannerPositonId}) > div:nth-child(5) > input[type=text]`
+    ).value;
 
-        //Choosing HTML element
-        let resultLinkBannerMasc = document.querySelector(`#pbc-banner-masc-${bannerPositonId} > a`)
-        let resultBannerDeskMasc = document.querySelector(`#pbc-banner-masc-${bannerPositonId} > a > picture > img`)
-        let resultBannerMobileMasc = document.querySelector(`#pbc-banner-masc-${bannerPositonId} > a > picture > source`)
+    //Choosing HTML element
+    let resultLinkBannerMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${bannerPositonId}) > a`
+    );
+    let resultBannerDeskMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${bannerPositonId}) > a > picture > img`
+    );
+    let resultBannerMobileMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${bannerPositonId}) > a > picture > source`
+    );
 
-        if (linkBannerMasc.length < 3 && bannerDeskMasc.length < 3 && bannerMobileMasc.length < 3 && altBannerMasc.length < 3) {
-            flag = 1
-            alert('Favor preencher todos os campos corretamente');         
-        } else {
-            resultLinkBannerMasc.href = linkBannerMasc
-            resultBannerDeskMasc.src = bannerDeskMasc
-            resultBannerDeskMasc.alt = altBannerMasc
-            resultBannerMobileMasc.srcset = bannerMobileMasc
-        }        
-        i++
-    }      
+    if (
+      linkBannerMasc.length < 3 &&
+      bannerDeskMasc.length < 3 &&
+      bannerMobileMasc.length < 3 &&
+      altBannerMasc.length < 3
+    ) {
+      flag = 1;
+      alert("Favor preencher todos os campos corretamente");
+    } else {
+      resultLinkBannerMasc.href = linkBannerMasc;
+      resultBannerDeskMasc.src = bannerDeskMasc;
+      resultBannerDeskMasc.alt = altBannerMasc;
+      resultBannerMobileMasc.srcset = bannerMobileMasc;
+    }
+    i++;
+  }
 }
 
 //Function to create form following banner's quantity
 window.onload = function buildHomeSlideForms() {
-    const homeSlideList = document.getElementsByClassName("box-banner")
-    const appendFormReference = document.querySelector('#homeSlideForm')
+  const homeSlideList = document.getElementsByClassName("box-banner");
+  const formBackground = document.getElementsByClassName("pbcForm");
+  const appendFormReference = document.querySelector("#homeSlideForm");
+  const hexBgForm = [
+    "#d9ed92",
+    "#b5e48c",
+    "#99d98c",
+    "#76c893",
+    "#52b69a",
+    "#588157",
+  ];
 
-    for (let i = 0; i < homeSlideList.length; i++) {
-        let attributesLinkBannerMasc = document.querySelector(`#pbc-banner-masc-${i+1} > a`)
-        let attributesBannerDeskMasc = document.querySelector(`#pbc-banner-masc-${i+1} > a > picture > img`)
-        let attributesBannerMobileMasc = document.querySelector(`#pbc-banner-masc-${i+1} > a > picture > source`)
-        let inputHtmlSection = `
-            <section>
-            <h2>Banner 0${i+1}</h2>        
-            <div>
-                <label for='linkBannerMasc${i+1}'>Link:</label>
-                <input id='linkBannerMasc${i+1}' type='text' value="${attributesLinkBannerMasc.href}" required></input>
-            </div>
-        
-            <div>
-                <label for='bannerDeskMasc${i+1}'>Imagem Desk:</label>
-                <input id='bannerDeskMasc${i+1}' type='text' value="${attributesBannerDeskMasc.src}" required></input>
-            </div>
-        
-            <div>
-                <label for='bannerMobileMasc${i+1}'>Imagem Mobile:</label>
-                <input id='bannerMobileMasc${i+1}' type='text' value="${attributesBannerMobileMasc.srcset}" required></input>
-            </div>
-        
-            <div>
-                <label for='altBannerMasc${i+1}'>Alt:</label>
-                <input id='altBannerMasc${i+1}' type='text' value="${attributesBannerDeskMasc.alt}" required></input>
-            </div>
-            </section>
-        `
-        appendFormReference.innerHTML += `${inputHtmlSection}\n`
-    }
+  for (let i = 0; i < homeSlideList.length; i++) {
+    let attributesLinkBannerMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${i + 1}) > a`
+    );
+    let attributesBannerDeskMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${i + 1}) > a > picture > img`
+    );
+    let attributesBannerMobileMasc = document.querySelector(
+      `.pdv-automation div:nth-child(${i + 1}) > a > picture > source`
+    );
+    let inputHtmlSection = `
+          <section class='pbcForm' draggable="true" data-index="${i}">
+          <h2>Banner 0${i + 1}</h2>        
+          <div>
+              <label>Link:</label>
+              <input type='text' value="${
+                attributesLinkBannerMasc.href
+              }" required></input>
+          </div>
+      
+          <div>
+              <label>Imagem Desk:</label>
+              <input type='text' value="${
+                attributesBannerDeskMasc.src
+              }" required></input>
+          </div>
+      
+          <div>
+              <label>Imagem Mobile:</label>
+              <input type='text' value="${
+                attributesBannerMobileMasc.srcset
+              }" required></input>
+          </div>
+      
+          <div>
+              <label>Alt:</label>
+              <input type='text' value="${
+                attributesBannerDeskMasc.alt
+              }" required></input>
+          </div>
+          </section>
+      `;
+    appendFormReference.innerHTML += `${inputHtmlSection}\n`;
+  }
+
+  for (let k = 0; k < formBackground.length; k++) {
+    formBackground[k].style.backgroundColor = hexBgForm[k];
+  }
+  draggFormElement();
+};
+
+function draggFormElement() {
+  const elements = document.querySelectorAll(".pbcForm");
+  let draggingElement;
+
+  elements.forEach((element) => {
+    element.addEventListener("dragstart", (e) => {
+      draggingElement = e.target;
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.setData("text/plain", null);
+      element.classList.add("dragging");
+    });
+
+    element.addEventListener("dragend", (e) => {
+      draggingElement = null;
+      element.classList.remove("dragging");
+    });
+
+    element.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    element.addEventListener("drop", (e) => {
+      e.preventDefault();
+
+      if (draggingElement === element) {
+        return;
+      }
+
+      const container = element.parentNode;
+      const draggingIndex = parseInt(draggingElement.dataset.index);
+      const targetIndex = parseInt(element.dataset.index);
+
+      if (draggingIndex < targetIndex) {
+        container.insertBefore(draggingElement, element.nextSibling);
+      } else {
+        container.insertBefore(draggingElement, element);
+      }
+
+      // Update the data-index attributes of all elements to reflect their new positions
+      const updatedElements = document.querySelectorAll(".pbcForm");
+      updatedElements.forEach((el, index) => {
+        el.dataset.index = index;
+      });
+    });
+  });
 }
